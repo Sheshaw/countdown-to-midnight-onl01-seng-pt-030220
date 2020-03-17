3fa8c53c9ade38edba3bb19b89584b2b65f538dc
Chayan Martinez <nayachmartinez68@gmail.com>
2020-03-17 22:01:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,10 +8,10 @@ def countdown(seconds)
   "HAPPY NEW YEAR!"
 end
 
- def countdown_with_sleep(sleep.seconds)
+ def countdown_with_sleep(seconds)
   while seconds > 0 && 
    puts "#{seconds} SECOND(S)!"
-  seconds -= 1
+  sleep.seconds -= 1
  end
   "HAPPY NEW YEAR!"
 end
